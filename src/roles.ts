205a403d7c967df783cec30203e455b78244f196
themx05 <maximiliencomlan05@gmail.com>
2020-09-27 18:35:46 +0100

    Added ScopedRoleList which assembles all roles into an array

--- src/roles.ts
+++ src/roles.ts
@@ -64,6 +64,74 @@ export type UserScope = UserProfileScope | UserBusinessScope | "user:.*:.*";
 export type SystemScope = SystemMethodScope | SystemMethodAccountScope | SystemAdminScope | SystemPropertyScope | SystemEventScope | SystemLogScope | "system:.*:.*";
 export type ScopedRole = UserScope | TicketScope | WalletScope | InvestmentNetworkScope | AdminRoleScope | SystemScope | ".*:.*:.*";
 
+
+export const ScopedRoleList: ScopedRole[] = [
+    "user:profile:read",
+    "user:profile:activate",
+    "user:profile:block",
+    "user:profile:.*",
+
+    "user:business:read",
+    "user:business:allow",
+    "user:business:block",
+    "user:business:delete",
+    "user:business:.*",
+
+    "ticket:details:read",
+    "ticket:status:approve",
+    "ticket:status:abort",
+    "ticket:transaction:read",
+    "ticket:status:.*",
+    "ticket:transaction:.*", // all tickets's transaction roles
+    "ticket:.*:.*",
+
+    "wallet:details:read",
+    "wallet:balance:credit",
+    "wallet:balance:debit",
+    "wallet:transfer:read",
+    "wallet:history:read", // all wallet history roles
+    "wallet:balance:.*",
+    "wallet:.*:.*",
+
+    "investment:network:read" ,
+    "investment:network:.*",
+
+    "system:method:read",
+    "system:method:create",
+    "system:method:update",
+    "system:method:delete",
+    "system:method:.*",
+
+    "system:methodaccount:read",
+    "system:methodaccount:create",
+    "system:methodaccount:update",
+    "system:methodaccount:delete",
+    "system:methodaccount:.*",
+
+    "system:property:read",
+    "system:property:update",
+    "system:property:.*",
+
+    "system:admin:read",
+    "system:admin:create",
+    "system:admin:disable",
+    "system:admin:enable",
+    "system:admin:.*",
+
+    "admin:role:read",
+    "admin:role:update",
+    "admin:role:*",
+
+    "system:event:read",
+    "system:log:read",
+
+    "user:.*:.*",
+
+    "system:.*:.*",
+
+    ".*:.*:.*"
+];
+
 export interface UserRole{
     id: string;
     roles: ScopedRole[];
