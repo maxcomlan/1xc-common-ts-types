bb3c48fdd19d5a1d18e27a126dcf190cb9417f9f
themx05 <maximiliencomlan05@gmail.com>
2020-10-13 13:56:57 +0100

    Added resume types

---
+++ src/resumes.ts
@@ -0,0 +1,17 @@
+import { Gender, WalletType } from "../core";
+
+export interface WalletResume{
+    code: string;
+    user: string;
+    type: WalletType;
+    currency: string;
+}
+
+export interface UserResume{
+    id: string;
+    firstName: string;
+    lastName: string;
+    gender: Gender;
+    email: string;
+    isMerchant: boolean;
+}
